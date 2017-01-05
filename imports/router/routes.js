934ef85d549cce9db2481530db9bcd7ebcd64177
igibbs@ostx.com <igibbs@ostx.com>
2017-01-05 17:30:30 -0500

    wip adding new login page

--- imports/router/routes.js
+++ imports/router/routes.js
@@ -1,11 +1,12 @@
 import Home from '../ui/pages/Home.jsx';
 import About from '../ui/pages/About.jsx';
 import NotFound from '../ui/pages/NotFound.jsx';
+import EmailPasswordForm from '../ui/components/forms/EmailPasswordForm.jsx';
 
 const routes = [
   {
     path: '/',
-    component: Home
+    component: EmailPasswordForm
   },
   {
     path: '/about',
